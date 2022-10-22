665e01dba0f666700a3348b8ba31bd7fdbbe6758
0Left <leftamail@gmail.com>
2022-10-22 12:25:22 -0300

    classes de portas e perguntas criados

--- Provinha/Assets/StateMachine.cs
+++ Provinha/Assets/StateMachine.cs
@@ -6,4 +6,42 @@ using UnityEngine;
 public class StateMachine : MonoBehaviour
 {
     
+    [SerializeField] public List<Porta> portas = new List<Porta>();
+
+    void Start()
+    {
+        callMe();
+    }
+    private void callMe()
+    {
+        foreach (var porta in portas)
+        {
+            Console.WriteLine(porta.pergunta.questionHeader);
+        }
+    }
+}
+[System.Serializable]
+public class Porta
+{
+    public Pergunta pergunta;
+    public int pontosMinimos;
+    public int pontosRecebidos;
+    [System.NonSerialized] public bool fuiRespondida;
+
+    public bool verificarRespostaCerta(int resposta)
+    {
+        return (resposta == pergunta.respostaCerta ? true : false);
+    }
+}
+[System.Serializable]
+public class Pergunta
+{
+    private const int MaxLength = 30; // for example
+    public string questionHeader;
+    [TextArea]public string questionBody;
+    public string resposta1;
+    public string resposta2;
+    public string resposta3;
+
+    [Range(1,3)] public int respostaCerta;
 }
